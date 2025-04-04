9cfb7ee212cd0eef7d1940791e6541fef5a49427
Liz <lizmarie.mendez86@gmail.com>
2025-04-04 00:39:22 -0400

    added CSS

--- InventoryFrontEnd/src/components/Navbar.jsx
+++ InventoryFrontEnd/src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <nav style={{ padding: '1rem', background: '#eee' }}>
-            <Link to="/" style={{ margin: '0 1rem' }}>Home</Link>
-            <Link to="/signup" style={{ margin: '0 1rem' }}>Sign Up</Link>
-            <Link to="/login" style={{ margin: '0 1rem' }}>Login</Link>
-            <Link to="/inventory" style={{ margin: '0 1rem' }}>Inventory</Link>
-      </nav>
-    ); 
+  return (
+    <nav style={{ padding: '1rem', backgroundColor: '#1f1f1f', color: '#e0e0e0' }}>
+      <Link to="/" style={{ margin: '0 1rem', color: '#e0e0e0', textDecoration: 'none' }}>Home</Link>
+      <Link to="/signup" style={{ margin: '0 1rem', color: '#e0e0e0', textDecoration: 'none' }}>Sign Up</Link>
+      <Link to="/login" style={{ margin: '0 1rem', color: '#e0e0e0', textDecoration: 'none' }}>Login</Link>
+      <Link to="/inventory" style={{ margin: '0 1rem', color: '#e0e0e0', textDecoration: 'none' }}>Inventory</Link>
+    </nav>
+  );
 }
 
 export default Navbar;
